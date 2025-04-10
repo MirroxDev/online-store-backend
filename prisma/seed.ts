74a685cfa1ef7f9f32e055febaa5674c16a12249
MirroXDev <vitaliy12392@gmail.com>
2025-04-10 15:48:20 +0300

    add stratagies , login, register user

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,17 +4,17 @@ const prisma: PrismaClient = new PrismaClient();
 
 async function main() {
   // Создаем страны
-  await prisma.countries.create({
-    data: {
-      country_code: 'ua',
-      name: 'Украина',
-      name_ua: 'Україна',
-      name_ru: 'Украина',
-      name_en: 'Ukraine',
-      name_de: 'Ukraine',
-      name_pl: 'Ukraina',
-    },
-  });
+  // await prisma.countries.create({
+  //   data: {
+  //     country_code: 'ua',
+  //     name: 'Украина',
+  //     name_ua: 'Україна',
+  //     name_ru: 'Украина',
+  //     name_en: 'Ukraine',
+  //     name_de: 'Ukraine',
+  //     name_pl: 'Ukraina',
+  //   },
+  // });
 
 
   console.log('Seed data created successfully!');
