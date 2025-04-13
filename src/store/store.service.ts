2fe8a87b0fb0d32375e8e0b8401fc382a9511a1a
MirroXDev <vitaliy12392@gmail.com>
2025-04-13 13:42:01 +0300

    stores finaly

--- src/store/store.service.ts
+++ src/store/store.service.ts
@@ -1,7 +1,54 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateStoreDto } from './dto/create-store.dto'
+import { UpdateStoreDto } from './dto/update-store.dto'
 
 @Injectable()
 export class StoreService {
-    constructor(private prisma: PrismaService) {}
+	constructor(private prisma: PrismaService) {}
+
+	async getById(storeId: string, userId: string) {
+		const store = await this.prisma.store.findUnique({
+			where: {
+				id: storeId,
+				userId
+			}
+		})
+
+		if (!store) throw new NotFoundException('Store not found')
+
+		return store
+	}
+
+	async create(dto: CreateStoreDto, userId: string) {
+		return this.prisma.store.create({
+			data: {
+				title: dto.title,
+				userId
+			}
+		})
+	}
+	async update(dto: UpdateStoreDto, userId: string, storeId: string) {
+		await this.getById(storeId, userId)
+
+		return this.prisma.store.update({
+			where: {
+				id: storeId
+			},
+			data: {
+				...dto,
+				userId
+			}
+		})
+    }
+    
+	async delete(userId: string, storeId: string) {
+		await this.getById(storeId, userId)
+
+		return this.prisma.store.delete({
+			where: {
+				id: storeId
+			}
+		})
+	}
 }
