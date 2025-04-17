e9172e7d539acd562fb87d596327c2d22cd4a098
MirroXDev <vitaliy12392@gmail.com>
2025-04-17 12:47:46 +0300

    finaly category, file

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+	constructor(private prisma: PrismaService) {}
+
+	async getAll(searchTerm?: string) {
+		if (searchTerm) return this.getSearchTermFilter(searchTerm)
+
+		const products = this.prisma.product.findMany({
+			orderBy: {
+				createdAt: 'desc'
+			},
+			include: {
+				category: true,
+				color: true,
+				rewiews: true
+			}
+		})
+
+		return products
+	}
+
+	private getSearchTermFilter(searchTerm: string) {
+		return {
+			OR: [
+				{
+					name: {
+						contains: searchTerm,
+						mode: 'insensitive'
+					}
+				},
+				{
+					description: {
+						contains: searchTerm,
+						mode: 'insensitive'
+					}
+				}
+			]
+		}
+	}
+}
